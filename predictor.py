5c02b9fbf6b017d0a427f6560dbbc704c36f6a94
annelericin <gokhankinas@hotmail.com>
2025-06-09 14:49:31 +0300

    Update predictor.py
    mhv

--- predictor.py
+++ predictor.py
@@ -1,11 +1,1 @@
-# predictor.py içine ekle
-import numpy as np
 
-def predict_classes(model, image_array):
-    predictions = model.predict(image_array)
-    results = {}
-    output_labels = ['labia_minora', 'labia_majora', 'clitoris']  # kendi sınıflarına göre düzenle
-    for i, label in enumerate(output_labels):
-        predicted_class = np.argmax(predictions[i])
-        results[label] = predicted_class
-    return results
